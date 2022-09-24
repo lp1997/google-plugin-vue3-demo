aa6bcc3df44b7324497c053bf53fa7037a7b10f6
lp <l1607002275@163.com>
2022-09-24 22:46:16 +0800

    del

--- src/utils/chromeUtils.js
+++ src/utils/chromeUtils.js
@@ -13,14 +13,6 @@ export function localRemove (key) {
   return window.chrome.storage.sync.remove(key)
 }
 
-export function getCurrentTabId (key) { // 获取当前页面id
-  return new Promise((resolve, reject) => {
-    window.chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      resolve(tabs.length ? tabs[0].windowId : null)
-    })
-  })
-}
-
 export function getPageInfo (key) { // 获取当前页面信息
   return new Promise((resolve) => {
     window.chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
