176c53034c817f61296bf81be573a8c26d204af8
lp <l1607002275@163.com>
2022-11-28 01:13:00 +0800

    修改

--- src/utils/chromeUtils.ts
+++ src/utils/chromeUtils.ts
@@ -2,29 +2,29 @@ declare namespace ReturnType {
   type id = number
 }
 // 信息存储
-export function localSet (key:string, val:string) {
+export const localSet = (key:string, val:string) => {
   window.chrome.storage.sync.set({ [key]: val })
 }
-export function localGet (key:string[]) {
+export const localGet = (key:string[]) => {
   return new Promise((resolve) => {
     window.chrome.storage.sync.get(key, (res:string) => {
       resolve(res)
     })
   })
 }
-export function localRemove (key:string) {
+export const localRemove = (key:string) => {
   return window.chrome.storage.sync.remove(key)
 }
 
-export function getCurrentTabId ():Promise<ReturnType.id> { // 获取当前页面id
+export const getCurrentTabId = ():Promise<ReturnType.id> => { // 获取当前页面id
   return new Promise((resolve) => {
-    window.chrome.tabs.query({ active: true, currentWindow: true }, function (tabs:[{id:number}]) {
+    window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs:[{id:number}]) => {
       resolve(tabs[0].id)
     })
   })
 }
 
-export function getPageInfo () { // 获取当前页面信息
+export const getPageInfo = () => { // 获取当前页面信息
   return new Promise((resolve) => {
     window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs:[{id:number}]) => {
       resolve(tabs)
@@ -32,50 +32,50 @@ export function getPageInfo () { // 获取当前页面信息
   })
 }
 
-export function sendData2Main (tabId:number, data:any, cb?:Function) { // popup/background发送消息到main,并处理返回的信息(main发送消息，先发给background，background再转发给popup)
-  window.chrome.tabs.sendMessage(tabId, data, function (res:any) {
+export const sendData2Main = (tabId:number, data:any, cb?:Function) => { // popup/background发送消息到main,并处理返回的信息(main发送消息，先发给background，background再转发给popup)
+  window.chrome.tabs.sendMessage(tabId, data, (res:any) => {
     cb && cb(res)
   })
 }
 
-export function receiveData (cb?:Function) { // 非popup接收信息
+export const receiveData = (cb?:Function) => { // 非popup接收信息
   window.chrome.extension.onMessage.addListener((req:any, sender:any, sendRes:any) => {
     cb && cb(req, sender, sendRes)
   })
 }
 
-export function sendData (data:any, cb?:Function) { // 非popup发送信息
+export const sendData = (data:any, cb?:Function) => { // 非popup发送信息
   window.chrome.runtime.sendMessage(data, (res:any) => { // 接收返回的消息
     cb && cb(res)
   })
 }
 
-export function receiveConnetData (cb?:Function) { // 接口信息获取
-  window.chrome.runtime.onConnect.addListener(function (port:any) {
+export const receiveConnetData = (cb?:Function) => { // 接口信息获取
+  window.chrome.runtime.onConnect.addListener((port:any) => {
     port.onMessage.addListener(cb)
 
-    port.onDisconnect.addListener(function (port:any) {
+    port.onDisconnect.addListener((port:any) => {
       port.onMessage.removeListener(cb)
     })
   })
 }
 
-export function getBackgroundData (data:any) { // popup获取background数据
+export const getBackgroundData = (data:any) => { // popup获取background数据
   return window.chrome.extension.getBackgroundPage()[data]
 }
 
-export function openNewPage (url:string) { // 页面打开新窗口链接
+export const openNewPage = (url:string) => { // 页面打开新窗口链接
   window.chrome.tabs.create({ url: url })
 }
 
-export function listenPageCreated (cb?:Function) { // 监听窗口新建
-  window.chrome.tabs.onCreated.addListener(function (res:string) {
+export const listenPageCreated = (cb?:Function) => { // 监听窗口新建
+  window.chrome.tabs.onCreated.addListener((res:string) => {
     cb && cb(res)
   })
 }
 
-export function listenPageActivated (cb?:Function) { // 监听窗口切换
-  window.chrome.tabs.onActivated.addListener(function (res:string) {
+export const listenPageActivated = (cb?:Function) => { // 监听窗口切换
+  window.chrome.tabs.onActivated.addListener((res:string) => {
     cb && cb(res)
   })
 }
